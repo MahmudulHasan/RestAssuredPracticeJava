1c698652567be96d015c9a6d3960545c97e832c5
MahmudulHasan <autul.aust@gmail.com>
2025-03-03 23:12:08 +0600

    Calculate sum of course prices and match

--- resutassuredjava/src/main/java/com/example/ComplexJsonParse.java
+++ resutassuredjava/src/main/java/com/example/ComplexJsonParse.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.testng.Assert;
+
 import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonParse {
@@ -16,12 +18,22 @@ public class ComplexJsonParse {
 
         int courseCount = js.getInt("courses.size()");
         System.out.println("Total number of courses: " + courseCount);
+        int totalAmount = js.getInt("dashboard.purchaseAmount");
+        int expectedTotalAmount = 0;
         
         for (int i = 0; i < courseCount; i++) {
             String title = js.getString("courses[" + i + "].title");
             int price = js.getInt("courses[" + i + "].price");
+            int copies = js.getInt("courses[" + i + "].copies");
             System.out.println("Course " + (i + 1) + " Title: " + title);
             System.out.println("Course " + (i + 1) + " Price: " + price);
-        }      
+            if(title.equalsIgnoreCase("Cypress")) {
+                System.out.println("Course " + (i + 1) + " Copies: " + copies);
+            }
+            expectedTotalAmount += price * copies;
+        }   
+        System.out.println("Expected Total Amount: " + expectedTotalAmount);
+        System.out.println("Actual Total Amount: " + totalAmount);   
+        Assert.assertEquals(totalAmount, expectedTotalAmount);
     }
 }
