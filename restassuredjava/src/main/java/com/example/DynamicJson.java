0fd48047fd41b16ec715f6f47cef16fe8993d2f8
MahmudulHasan <autul.aust@gmail.com>
2025-03-05 00:14:01 +0600

    Added Dataprovider to run multiple test

--- restassuredjava/src/main/java/com/example/DynamicJson.java
+++ restassuredjava/src/main/java/com/example/DynamicJson.java
@@ -5,19 +5,25 @@ import static io.restassured.RestAssured.*;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 
 public class DynamicJson {
-    @Test
-    public void addBook() {
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
         String response = given().header("Content-Type","application/json").
-        body(payload.AddBook("abssdf", "123s423")).
+        body(payload.AddBook(isbn, aisle)).
         when().post("/Library/Addbook.php").
         then().assertThat().statusCode(200)
         .extract().response().asString();
         JsonPath js = ReUsableMethods.rawToJson(response);
         String id = js.getString("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData() {
+        return new Object[][] {{"asdf", "1234"}, {"qwer", "5678"}, {"zxcv", "9012"}};
     }
     
 }
